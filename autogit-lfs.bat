ee1d24e03522a16f73ac6d277469b6accc6b35fc
Dee <dee.covan@gmail.com>
2025-12-10 15:21:43 +0200

    Add autogit-lfs.bat

---
+++ autogit-lfs.bat
@@ -0,0 +1,10 @@
+git lfs install
+git lfs track *.zip
+git lfs track *.7z
+git lfs track *.glb
+git lfs track *.wav
+git lfs track *.pck
+git lfs track *.exe
+git lfs track *.pdf
+git lfs track *.psd
+git add .gitattributes -f
