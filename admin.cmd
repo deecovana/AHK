5744abd85fe6d615726ac17069b096657f0d9899
Dee <dee.covan@gmail.com>
2026-03-07 23:04:27 +0200

    Up: admin.cmd up.

--- admin.cmd
+++ admin.cmd
@@ -1,4 +1,5 @@
 net user administrator /active:yes
 @echo Now Press Win+L and enter as Administrator (Left Bottom on the screen)
-@echo ......................................................................
+@echo Finish You dirty business and press any key...........................
 @pause
+net user administrator /active:no
