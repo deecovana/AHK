da4af745f014ba561ca4691c1e21c887ef17ca1a
Dee <dee.covan@gmail.com>
2026-03-26 12:01:35 +0200

    Up: autogit-lfs-delete-all.bat

--- autogit-lfs-delete-all.bat
+++ autogit-lfs-delete-all.bat
@@ -6,3 +6,6 @@ git add .gitattributes
 git commit -m "Stop tracking LFS files"
 git push origin --all --force
 git push origin --tags --force
+git lfs ls-files
+git lfs prune
+git rm -rf .git/lfs/objects
